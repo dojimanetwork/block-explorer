f847b77136bf0f58939cab4d0d9fbcc127a9ec5e
vijay04 <vijay.prasad@dojima.network>
2022-11-23 21:46:32 +0530

    removed unused imports

--- src/components/common/no.data.text.tsx
+++ src/components/common/no.data.text.tsx
@@ -1,6 +1,6 @@
 import { Theme, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import { vpx10, vpx12 } from "../../constants/px.vh";
+import { vpx12 } from "../../constants/px.vh";
 import VerticalFlex from "./vertical.flex";
 
 interface LoaderIfc {

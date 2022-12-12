dc68658e835cbd8d410509de418cd75d86ea7cd6
vijay04 <vijay.prasad@dojima.network>
2022-12-12 10:17:42 +0530

    changed tag name in sh file

--- deploy-dojima-block-explorer.sh
+++ deploy-dojima-block-explorer.sh
@@ -1,6 +1,6 @@
 docker build --platform=linux/amd64 -f Dockerfile --force-rm  -t dojima-block-explorer .
 
-docker tag dojima-landing-page:latest 576263512135.dkr.ecr.$REGION.amazonaws.com/dojima-block-explorer:$TAG
+docker tag dojima-block-explorer:latest 576263512135.dkr.ecr.$REGION.amazonaws.com/dojima-block-explorer:$TAG
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin 576263512135.dkr.ecr.$REGION.amazonaws.com
 
